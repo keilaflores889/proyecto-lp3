8d90869bc0b3f5192a6096d899858b02d357cf66
Keila Andrea Flores Garcete <keilaflores889@gmail.com>
2024-09-06 10:52:19 -0400

    feat: uso de dropdown, url_for en el menú, ordenar la vista, conexion a la bd, dao

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-#importar referenciales
+# importar referenciales
 from app.rutas.referenciales.ciudad.ciudad_routes import ciumod
 
-#registrar referenciales
-app.register_blueprint(ciumod, url_prefix='/ciudad')
-
+# registrar referenciales
+modulo0 = '/referenciales'
+app.register_blueprint(ciumod, url_prefix=f'{modulo0}/ciudad')
