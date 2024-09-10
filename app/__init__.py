34a5c5a8f42f52217cad25011635001ca5782215
Keila Andrea Flores Garcete <keilaflores889@gmail.com>
2024-09-10 08:33:08 -0400

    agregando-Lista-Pais

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 # importar referenciales
 from app.rutas.referenciales.ciudad.ciudad_routes import ciumod
-
+from app.rutas.referenciales.paises.pais_routes import paimod
 # registrar referenciales
 modulo0 = '/referenciales'
 app.register_blueprint(ciumod, url_prefix=f'{modulo0}/ciudad')
+app.register_blueprint(paimod, url_prefix=f'{modulo0}/paises')
