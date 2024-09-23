e1c2268941c5d92ebb3c755e4e9e5ef0bbbf82a8
Keila Andrea Flores Garcete <keilaflores889@gmail.com>
2024-09-22 20:32:32 -0400

    agregado referencial "Teléfono"

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,8 @@ from app.rutas.referenciales.ciudad.ciudad_routes import ciumod #ciudad
 from app.rutas.referenciales.paises.pais_routes import paimod   #pais
 from app.rutas.referenciales.correo.correo_routes import cormod #correo
 from app.rutas.referenciales.direccion.direccion_routes import diremod #direccion
+from app.rutas.referenciales.telefono.telefono_routes import telmod  #telefono
+
 
 # registrar referenciales
 modulo0 = '/referenciales'
@@ -14,7 +16,7 @@ app.register_blueprint(ciumod, url_prefix=f'{modulo0}/ciudad') #ciudad
 app.register_blueprint(paimod, url_prefix=f'{modulo0}/paises') #pais
 app.register_blueprint(cormod, url_prefix=f'{modulo0}/correo') #correo
 app.register_blueprint(diremod, url_prefix=f'{modulo0}/direccion') #direccion
-
+app.register_blueprint(telmod, url_prefix=f'{modulo0}/direccion')  #telefono
 
 #ciudad
 from app.rutas.referenciales.ciudad.ciudad_api import ciuapi
@@ -27,6 +29,9 @@ from app.rutas.referenciales.correo.correo_api import correoapi
 
 #direccion
 from.rutas.referenciales.direccion.direccion_api import direapi
+
+#telefono
+from app.rutas.referenciales.telefono.telefono_api import telapi
 
 # APIS v1
 #Ciudad
@@ -45,6 +50,6 @@ app.register_blueprint(correoapi, url_prefix=version1)
 version1 = '/api/v1'
 app.register_blueprint(direapi, url_prefix=version1)
 
-
-
-
+#telefono
+version1 = '/api/v1'
+app.register_blueprint(telapi, url_prefix=version1)
