02c01adc53d9a9cf89789a20ba2273bdbbc4af9f
Keila Andrea Flores Garcete <keilaflores889@gmail.com>
2024-09-26 13:28:36 -0400

    .

--- app/__init__.py
+++ app/__init__.py
@@ -5,40 +5,28 @@ app = Flask(__name__)
 # importar referenciales
 from app.rutas.referenciales.ciudad.ciudad_routes import ciumod #ciudad
 from app.rutas.referenciales.paises.pais_routes import paimod   #pais
-from app.rutas.referenciales.correo.correo_routes import cormod #correo
-from app.rutas.referenciales.direccion.direccion_routes import diremod #direccion
-from app.rutas.referenciales.telefono.telefono_routes import telmod  #telefono
 from app.rutas.referenciales.nacionalidad.nacionalidad_routes import naciomod  #nacionalidad
 from app.rutas.referenciales.ocupacion.ocupacion_routes import ocupmod  #ocupacion
 from app.rutas.referenciales.estado_civil.estado_civil_routes import estacivmod  #estado civil
 from app.rutas.referenciales.sexo.sexo_routes import sexmod  #sexo
 
+
+
 # registrar referenciales
 modulo0 = '/referenciales'
 app.register_blueprint(ciumod, url_prefix=f'{modulo0}/ciudad') #ciudad
 app.register_blueprint(paimod, url_prefix=f'{modulo0}/paises') #pais
-app.register_blueprint(cormod, url_prefix=f'{modulo0}/correo') #correo
-app.register_blueprint(diremod, url_prefix=f'{modulo0}/direccion') #direccion
-app.register_blueprint(telmod, url_prefix=f'{modulo0}/direccion')  #telefono
 app.register_blueprint(naciomod, url_prefix=f'{modulo0}/nacionalidad')  #nacionalidad
 app.register_blueprint(ocupmod, url_prefix=f'{modulo0}/ocupacion')  #ocupacion
 app.register_blueprint(estacivmod, url_prefix=f'{modulo0}/estadocivil')  #estado civil
 app.register_blueprint(sexmod, url_prefix=f'{modulo0}/sexo')  #sexo
+
  
 #ciudad
 from app.rutas.referenciales.ciudad.ciudad_api import ciuapi
 
 #pais
 from app.rutas.referenciales.paises.pais_api import paisapi
-
-#coreo
-from app.rutas.referenciales.correo.correo_api import correoapi
-
-#direccion
-from.rutas.referenciales.direccion.direccion_api import direapi
-
-#telefono
-from app.rutas.referenciales.telefono.telefono_api import telapi
 
 #nacionalidad
 from app.rutas.referenciales.nacionalidad.nacionalidad_api import nacioapi
@@ -52,6 +40,8 @@ from app.rutas.referenciales.estado_civil.estado_civil_api import estacivapi
 #sexo
 from app.rutas.referenciales.sexo.sexo_api import sexapi
 
+
+
 # APIS v1
 #Ciudad
 version1 = '/api/v1'
@@ -60,18 +50,6 @@ app.register_blueprint(ciuapi, url_prefix=version1)
 #Pais
 version1 = '/api/v1'
 app.register_blueprint(paisapi, url_prefix=version1)
-
-#correo
-version1 = '/api/v1'
-app.register_blueprint(correoapi, url_prefix=version1)
-
-#direccion
-version1 = '/api/v1'
-app.register_blueprint(direapi, url_prefix=version1)
-
-#telefono
-version1 = '/api/v1'
-app.register_blueprint(telapi, url_prefix=version1)
 
 #nacionalidad
 version1 = '/api/v1'
--- app/rutas/referenciales/direccion/direccion_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-diremod = Blueprint('direccion', __name__, template_folder='templates')
-
-@diremod.route('/direccion-index')
-def direccionIndex():
-    return render_template('direccion-index.html')
