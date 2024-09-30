14d7ada0a5070772299833f0f610b971639c0f42
Keila Andrea Flores Garcete <keilaflores889@gmail.com>
2024-09-30 13:01:13 -0400

    agregado referencial "Duración de la consulta"

--- app/__init__.py
+++ app/__init__.py
@@ -13,6 +13,7 @@ from app.rutas.referenciales.estado_cita.estado_cita_routes import estacitmod  #
 from app.rutas.referenciales.persona.persona_routes import persmod  #persona
 from app.rutas.referenciales.especialidad.especialidad_routes import especimod  #especialidad
 from app.rutas.referenciales.dia.dia_routes import diamod  #dia
+from app.rutas.referenciales.duracion_consulta.duracion_consulta_routes import duraconsumod  #duracion de la consulta
 
 # registrar referenciales
 modulo0 = '/referenciales'
@@ -26,6 +27,7 @@ app.register_blueprint(estacitmod, url_prefix=f'{modulo0}/estadocita')  #estado 
 app.register_blueprint(persmod, url_prefix=f'{modulo0}/persona') #persona
 app.register_blueprint(especimod, url_prefix=f'{modulo0}/especialidad') #especialidad
 app.register_blueprint(diamod, url_prefix=f'{modulo0}/dia') #dia
+app.register_blueprint(duraconsumod, url_prefix=f'{modulo0}/duracionconsulta') #duracion de la consulta
 
 #ciudad
 from app.rutas.referenciales.ciudad.ciudad_api import ciuapi
@@ -56,6 +58,9 @@ from app.rutas.referenciales.especialidad.especialidad_api import especiapi
 
 #dia
 from app.rutas.referenciales.dia.dia_api import diaapi
+
+#duracion de la consulta
+from app.rutas.referenciales.duracion_consulta.duracion_consulta_api import duraconsuapi
 
 # APIS v1
 #Ciudad
@@ -97,3 +102,7 @@ app.register_blueprint(especiapi, url_prefix=version1)
 #dia
 version1 = '/api/v1'
 app.register_blueprint(diaapi, url_prefix=version1)
+
+#duracion de la consulta
+version1 = '/api/v1'
+app.register_blueprint(duraconsuapi, url_prefix=version1)
