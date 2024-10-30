cea5d5714afaf245137827df99de227adee1d42d
Keila Andrea Flores Garcete <keilaflores889@gmail.com>
2024-10-30 09:32:36 -0300

    Cambios

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ from app.rutas.referenciales.paises.pais_routes import paimod   #pais
 from app.rutas.referenciales.nacionalidad.nacionalidad_routes import naciomod  #nacionalidad
 from app.rutas.referenciales.ocupacion.ocupacion_routes import ocupmod  #ocupacion
 from app.rutas.referenciales.estado_civil.estado_civil_routes import estacivmod  #estado civil
-from app.rutas.referenciales.sexo.sexo_routes import sexmod  #sexo
+
 from app.rutas.referenciales.estado_cita.estado_cita_routes import estacitmod  #estado de la cita
 from app.rutas.referenciales.persona.persona_routes import persmod  #persona
 from app.rutas.referenciales.especialidad.especialidad_routes import especimod  #especialidad
@@ -37,7 +37,7 @@ app.register_blueprint(paimod, url_prefix=f'{modulo0}/paises') #pais
 app.register_blueprint(naciomod, url_prefix=f'{modulo0}/nacionalidad')  #nacionalidad
 app.register_blueprint(ocupmod, url_prefix=f'{modulo0}/ocupacion')  #ocupacion
 app.register_blueprint(estacivmod, url_prefix=f'{modulo0}/estadocivil')  #estado civil
-app.register_blueprint(sexmod, url_prefix=f'{modulo0}/sexo')  #sexo
+
 app.register_blueprint(estacitmod, url_prefix=f'{modulo0}/estadocita')  #estado de la cita
 app.register_blueprint(persmod, url_prefix=f'{modulo0}/persona') #persona
 app.register_blueprint(especimod, url_prefix=f'{modulo0}/especialidad') #especialidad
@@ -68,8 +68,7 @@ from app.rutas.referenciales.ocupacion.ocupacion_api import ocupapi
 #estado civil
 from app.rutas.referenciales.estado_civil.estado_civil_api import estacivapi
 
-#sexo
-from app.rutas.referenciales.sexo.sexo_api import sexapi
+
 
 #estado de la cita
 from app.rutas.referenciales.estado_cita.estado_cita_api import estacitapi
@@ -124,9 +123,7 @@ app.register_blueprint(ocupapi, url_prefix=apiversion1)
 apiversion1 = '/api/v1'
 app.register_blueprint(estacivapi, url_prefix=apiversion1)
 
-#sexo
-apiversion1 = '/api/v1'
-app.register_blueprint(sexapi, url_prefix=apiversion1)
+
 
 #Estado de la cita
 apiversion1 = '/api/v1'
